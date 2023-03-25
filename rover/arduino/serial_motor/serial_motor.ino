17135fb9298897fd82f5ef57796c1855cca81f4f
Andrew Kosikowski <kosikoaj@rose-hulman.edu>
2023-03-25 13:15:06 -0400

    I did stuff

---
+++ rover/arduino/serial_motor/serial_motor.ino
@@ -0,0 +1,92 @@
+#include <Servo.h> 
+String command;
+//Unit Circle :)
+int ipn[4] = {2, 5, 4, 3};
+// pin 5 front left 
+// pin 2 front right
+// pin back 4 back left / middle
+// pin 3 back right middle
+int state[4] = {1, 0, 1, 0};
+char charBuf[128];
+
+
+
+
+
+
+Servo rightFrontServo;
+Servo leftFrontServo;
+Servo leftBackServo;
+Servo rightBackServo;
+
+size_t wheelCommandBufferSize =  1;
+size_t opCodeSize = 1;
+
+void setup() {
+  for(int i = 0; i < 4; i++) {
+    pinMode(ipn[i], OUTPUT);
+  }
+  Serial.begin(9600);
+  Serial.setTimeout(100); //in milliseconds
+  rightFrontServo.attach(ipn[0]);
+  leftFrontServo.attach(ipn[1]);
+  leftBackServo.attach(ipn[2]);
+  rightBackServo.attach(ipn[3]);
+  
+}
+
+//Debugging, printing is iffy regardless, can just be safer to use Serial.write
+void printBufferIntArray(uint8_t* array, size_t bufferSize){ 
+  Serial.print("[");
+  for(int i = 0; i < bufferSize; i++){
+    Serial.print(array[i]);
+    Serial.print(",");
+  }
+  
+  Serial.print("]\n");
+}
+
+void moveLeftWheel(uint8_t speed){
+  leftFrontServo.write(speed);
+  leftBackServo.write(speed);
+}
+
+// uint8_t convertSpeedtoProperRange(uint8_t speed){
+//   float converted = speed*180.0;
+//   uint8_t result = (uint8_t) converted/255;
+//   return result;
+// }
+
+void moveRightWheel(uint8_t speed){
+  rightFrontServo.write(speed);
+  rightBackServo.write(speed);
+}
+
+void loop() {
+
+  if (Serial.available() > 0){
+    uint8_t opBuffer[opCodeSize];
+    Serial.readBytes(opBuffer, opCodeSize); //Reads as many bytes or until port closes
+    Serial.write(opBuffer, opCodeSize); // Sends back to server to confirm correct received
+    if(opBuffer[0] == 0){
+       if (Serial.available() >= wheelCommandBufferSize*2) {
+          uint8_t leftWheelBuffer[wheelCommandBufferSize];
+          Serial.readBytes(leftWheelBuffer, wheelCommandBufferSize); //Reads as many bytes or until port closes
+          Serial.write(leftWheelBuffer, wheelCommandBufferSize); // Sends back to server to confirm correct received
+          uint8_t rightWheelBuffer[wheelCommandBufferSize];
+          Serial.readBytes(rightWheelBuffer, wheelCommandBufferSize); //Reads as many bytes or until port closes
+          Serial.write(rightWheelBuffer, wheelCommandBufferSize); // Sends back to server to confirm correct received
+
+
+          uint8_t convertedLeftSpeed = leftWheelBuffer[0];
+          uint8_t convertedRightSpeed = rightWheelBuffer[0];
+          // uint8_t convertedLeftSpeed = convertSpeedToProperRange(leftWheelBuffer[0]);
+          // uint8_t convertedRightSpeed = convertSpeedToProperRange(rightWheelBuffer[0]);
+          
+          moveLeftWheel(convertedLeftSpeed);
+          moveRightWheel(convertedRightSpeed);
+      }
+    }
+  }
+  
+}
--- rover/server.py
+++ rover/server.py
@@ -1,23 +1,52 @@
 from flask import Flask, request
-# import serial
+import serial
 import time
+import atexit
+# import os
 
-# ser = serial.Serial('/dev/cu.usbmodem2101', 9600, timeout=1)
+
+# ser = serial.Serial('/dev/cu.usbmodem2101', 9600, timeout=1) #Maybe good for linux stuff
+# ser = serial.Serial('COM3', 9600, timeout=1) #Windows 
+ser = serial.Serial()
+ser.setDTR(False)
+ser.port = 'COM3'
+ser.baudrate = 9600
+ser.timeout = 5
+ser.bytesize = serial.EIGHTBITS
+ser.stopbits = serial.STOPBITS_ONE
+ser.parity =serial.PARITY_NONE
+ser.close()
+ser.open()
+
+# print(ser.is_open)
+# ser.close()
+
+
 # print(ser.portstr)
 
-app = Flask(__name__)
 
-@app.route('/post_json', methods=['GET'])
+# app = Flask(__name__)
+
+
+
+def exit_handler():
+    ser.flush() #flushes and closes on exit
+    ser.close()
+
+atexit.register(exit_handler)
+
+# @app.route('/post_json', methods=['GET'])
 def hello_world():
-	val = 200
-	outp = bytes(f"{val}\n", 'utf-8')
-	print(outp)
-	# ser.write(outp)
-	val ^= 51400
-	print('here')
-	return "<p>Hello, World!</p>"
+    print(ser.is_open) #confirms port is open
+    data = [1,2,3,4,5,6,7,8] # Values from 0 - 255 allowed in each entry
+    bytesToSend = bytes(data)
+    print(bytesToSend)
+    ser.write(bytesToSend)
+    result = ser.read(8)
+    print(result)
+    print(result == bytesToSend) # Verify received same thing as sent
 
-@app.route('/wheel_command', methods=['GET'])
+# @app.route('/wheel_command', methods=['GET'])
 def process_json():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
@@ -27,3 +56,7 @@ def process_json():
         return json
     else:
         return 'Content-Type not supported!'
+
+
+if __name__ == "__main__":
+    hello_world()
