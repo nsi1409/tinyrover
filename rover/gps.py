c8034f9a4a673ccf1b6254ace3f3b07988398f13
rhit-choij4 <83384979+rhit-choij4@users.noreply.github.com>
2023-04-01 11:58:15 -0400

    GPS auto update does work now
    Pushing what i have

--- rover/gps.py
+++ rover/gps.py
@@ -6,7 +6,7 @@ import csv
 import sys
 
 def parse(): 
-    gps = serial.Serial("COM5", 4800)
+    gps = serial.Serial("COM8", 4800)
     inpt = gps.readline()
     csv_inpt = inpt.decode('utf-8').splitlines()
     csv_parse = csv.reader(csv_inpt)
@@ -18,20 +18,33 @@ turbo = Turbo(app)
 # gps = serial.Serial("COM5", baudrate=4800, xonxoff=0,rtscts=0,bytesize=8,stopbits=1,parity=serial.PARITY_NONE)
 parse_outp = parse()
 
-
+north = 0
+west = 0
 
 
 def min2dec(inpt):
-	finpt = float(inpt)
-	outp = (finpt // 100) + ((finpt%100)/60)
-	return outp
+    if(inpt == ''):
+        return "No"
+    else:
+        finpt = float(inpt)
+        outp = (finpt // 100) + ((finpt%100)/60)
+        return outp
+
 
 def returnCord():
-    if(parse_outp[0][0] == "$GPGGA"):
-        print(f'This is GPGGA')
-        print(parse_outp)
+    # print(parse_outp)
+    # print(f'Stuff{parse_outp[0][0]}')
+    if(parse_outp == [[]]):
+        return
+    elif(parse_outp[0][0] == "$GPGGA"):
+        # print(f'This is GPGGA')
         north = min2dec(parse_outp[0][2])
         west = min2dec(parse_outp[0][4])
+        print(f'North is {north}')
+        print(f'West is {west}')
+        if(north =="No" or west =="No"):
+            print(f'Not yet')
+            return
         print(f'North is {north}')
         print(f'West is {west}')
 
@@ -52,7 +65,7 @@ def trial():
 
 
 
-    return 'Hello World'
+    return 'North is' + north + 'West is' + west
 
 
 if __name__ == '__main__':
