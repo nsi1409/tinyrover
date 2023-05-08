847c3dedf2d74eba197a365c6d84a854f58c27b0
Evan <obrienem@rose-hulman.edu>
2023-05-08 02:57:52 -0400

    reorganizing; use rover2arduino to talk to the arduino

--- rover/rover2arduino.py
+++ rover/rover2arduino.py
@@ -1,37 +1,81 @@
 import serial
-import time
 import atexit
 
-class Controller:
-	def __init__(self):
-		self.ser = serial.Serial()
-		self.ser.setDTR(False)
-		self.ser.port = 'COM3'
-		self.ser.baudrate = 9600
-		self.ser.timeout = 5
-		self.ser.bytesize = serial.EIGHTBITS
-		self.ser.stopbits = serial.STOPBITS_ONE
-		self.ser.parity =serial.PARITY_NONE
-		self.ser.close()
-		self.ser.open()
-		# ser = serial.Serial('/dev/cu.usbmodem2101', 9600, timeout=1) #Maybe good for linux stuff
-		# ser = serial.Serial('COM3', 9600, timeout=1) #Windows 
+def exit_handler_wheels():
+    ser.flush() #flushes and closes on exit
+    ser.close()
 
-	def exit_handler():
-		self.ser.flush() #flushes and closes on exit
-		self.ser.close()
+def setup_wheels():
+	# ser = serial.Serial('/dev/cu.usbmodem2101', 9600, timeout=1) #Maybe good for linux stuff
+	# ser = serial.Serial('COM3', 9600, timeout=1) #Windows 
+	ser = serial.Serial()
+	ser.setDTR(False)
+	ser.port = 'COM4'
+	ser.baudrate = 9600
+	ser.timeout = 5
+	ser.bytesize = serial.EIGHTBITS
+	ser.stopbits = serial.STOPBITS_ONE
+	ser.parity =serial.PARITY_NONE
+	ser.close()
+	ser.open()
+	atexit.register(exit_handler_wheels)
 
-	def send2wheels():
-		print(ser.is_open) #confirms port is open
-		data = [1,2,3,4,5,6,7,8] # Values from 0 - 255 allowed in each entry
-		bytesToSend = bytes(data)
-		print(bytesToSend)
-		ser.write(bytesToSend)
-		result = ser.read(8)
-		print(result)
-		print(result == bytesToSend) # Verify received same thing as sent
+def test_both_wheels():
+    # print(ser.is_open) #confirms port is open
+	data = [0,100,100] # Values from 0 - 255 allowed in each entry
+	bytesToSend = bytes(data)
+	print(bytesToSend)
+	ser.write(bytesToSend)
+	result = ser.read(3)
+	print(result)
+	print(result == bytesToSend) # Verify received same thing as sent
+
+def test_right_wheels():
+    # print(ser.is_open) #confirms port is open
+	data = [2,100] # Values from 0 - 255 allowed in each entry
+	bytesToSend = bytes(data)
+	print(bytesToSend)
+	ser.write(bytesToSend)
+	result = ser.read(2)
+	print(result)
+	print(result == bytesToSend) # Verify received same thing as sent
+
+def test_left_wheels():
+	# print(ser.is_open) #confirms port is open
+	data = [1,100] # Values from 0 - 255 allowed in each entry
+	bytesToSend = bytes(data)
+	print(bytesToSend)
+	ser.write(bytesToSend)
+	result = ser.read(2)
+	print(result)
+	print(result == bytesToSend) # Verify received same thing as sent
+
+def test_left_wheels(speed):
+	# print(ser.is_open) #confirms port is open
+	data = [1,speed] # Values from 0 - 255 allowed in each entry
+	bytesToSend = bytes(data)
+	print(bytesToSend)
+	ser.write(bytesToSend)
+	result = ser.read(2)
+	print(result)
+	print(result == bytesToSend) # Verify received same thing as sent
+
+def test_right_wheels(speed):
+    # print(ser.is_open) #confirms port is open
+	data = [2,speed] # Values from 0 - 255 allowed in each entry
+	bytesToSend = bytes(data)
+	print(bytesToSend)
+	ser.write(bytesToSend)
+	result = ser.read(2)
+	print(result)
+	print(result == bytesToSend) # Verify received same thing as sent
+
+
 
 if __name__ == "__main__":
-	controller = Controller()
-	atexit.register(controller.exit_handler)
-	controller.send2wheels()
+	print("commented out :)")
+	# send2wheels(4, 7)
+	# for i in range(10):
+	# 	test_both_wheels()
+	# 	test_left_wheels()
+	# 	test_right_wheels()
--- rover/server.py
+++ rover/server.py
@@ -1,22 +1,9 @@
 from flask import Flask, request
-import time
-from rover2arduino import Controller
+import rover2arduino
 import socket
-import platform
-import sys
-sys.path.insert(0, 'C:/RHIT Rover Local Copy/tinyrover/user')
-import WheelCommandDebug
-app = Flask(__name__)
 
-@app.route('/post_json', methods=['GET'])
-def hello_world():
-	val = 200
-	outp = bytes(f"{val}\n", 'utf-8')
-	print(outp)
-	# ser.write(outp)
-	val ^= 51400
-	print('here')
-	return "<p>Hello, World!</p>"
+app = Flask(__name__)
+#setup_wheels()
 
 @app.route('/wheel_command', methods=['GET'])
 def process_json():
@@ -31,6 +18,4 @@ def process_json():
         return 'Content-Type not supported!'
 
 if __name__ == '__main__':
-    WheelCommandDebug.test_both_wheels() # prints b'\x00dd', whatever this is
-app.run(host='0.0.0.0', port=8080, debug=True, threaded=False) # if you don't indent this line like this, it gives an error ¯\_(ツ)_/¯
-        
+	app.run(host='0.0.0.0', port=8080, debug=True, threaded=False)
