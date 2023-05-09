b056e8928e8dcefec3621877b9943422283e1571
Evan <obrienem@rose-hulman.edu>
2023-05-08 20:23:38 -0400

    wheel controls

--- rover/rover2arduino.py
+++ rover/rover2arduino.py
@@ -1,81 +1,39 @@
 import serial
 import atexit
+import platform
 
-def exit_handler_wheels():
-    ser.flush() #flushes and closes on exit
-    ser.close()
+class Controller():
+	def __init__(self):
+		self.ser = serial.Serial()
+		self.ser.setDTR(False)
+		if platform.system() == 'Linux':
+			self.ser.port = '/dev/ttyACM0'
+		elif platform.system() == 'Darwin': # Darwin is MacOS
+			self.ser.port = '/dev/cu.usbmodem2101'
+		elif platform.system() == 'Windows':
+			self.ser.port = 'COM4'
+		self.ser.baudrate = 9600
+		self.ser.timeout = 5
+		self.ser.bytesize = serial.EIGHTBITS
+		self.ser.stopbits = serial.STOPBITS_ONE
+		self.ser.parity = serial.PARITY_NONE
+		self.ser.close()
+		self.ser.open()
+		atexit.register(self.exit_handler_wheels)
 
-def setup_wheels():
-	# ser = serial.Serial('/dev/cu.usbmodem2101', 9600, timeout=1) #Maybe good for linux stuff
-	# ser = serial.Serial('COM3', 9600, timeout=1) #Windows 
-	ser = serial.Serial()
-	ser.setDTR(False)
-	ser.port = 'COM4'
-	ser.baudrate = 9600
-	ser.timeout = 5
-	ser.bytesize = serial.EIGHTBITS
-	ser.stopbits = serial.STOPBITS_ONE
-	ser.parity =serial.PARITY_NONE
-	ser.close()
-	ser.open()
-	atexit.register(exit_handler_wheels)
+	def exit_handler_wheels(self):
+		self.ser.flush() #flushes and closes on exit
+		self.ser.close()
 
-def test_both_wheels():
-    # print(ser.is_open) #confirms port is open
-	data = [0,100,100] # Values from 0 - 255 allowed in each entry
-	bytesToSend = bytes(data)
-	print(bytesToSend)
-	ser.write(bytesToSend)
-	result = ser.read(3)
-	print(result)
-	print(result == bytesToSend) # Verify received same thing as sent
-
-def test_right_wheels():
-    # print(ser.is_open) #confirms port is open
-	data = [2,100] # Values from 0 - 255 allowed in each entry
-	bytesToSend = bytes(data)
-	print(bytesToSend)
-	ser.write(bytesToSend)
-	result = ser.read(2)
-	print(result)
-	print(result == bytesToSend) # Verify received same thing as sent
-
-def test_left_wheels():
-	# print(ser.is_open) #confirms port is open
-	data = [1,100] # Values from 0 - 255 allowed in each entry
-	bytesToSend = bytes(data)
-	print(bytesToSend)
-	ser.write(bytesToSend)
-	result = ser.read(2)
-	print(result)
-	print(result == bytesToSend) # Verify received same thing as sent
-
-def test_left_wheels(speed):
-	# print(ser.is_open) #confirms port is open
-	data = [1,speed] # Values from 0 - 255 allowed in each entry
-	bytesToSend = bytes(data)
-	print(bytesToSend)
-	ser.write(bytesToSend)
-	result = ser.read(2)
-	print(result)
-	print(result == bytesToSend) # Verify received same thing as sent
-
-def test_right_wheels(speed):
-    # print(ser.is_open) #confirms port is open
-	data = [2,speed] # Values from 0 - 255 allowed in each entry
-	bytesToSend = bytes(data)
-	print(bytesToSend)
-	ser.write(bytesToSend)
-	result = ser.read(2)
-	print(result)
-	print(result == bytesToSend) # Verify received same thing as sent
-
-
+	def test_both_wheels(self, data):
+		#print(ser.is_open) #confirms port is open
+		#data = [0,100,100] # Values from 0 - 255 allowed in each entry
+		bytesToSend = bytes(data)
+		print(bytesToSend)
+		self.ser.write(bytesToSend)
+		result = self.ser.read(3)
+		print(result)
+		print(result == bytesToSend) # Verify received same thing as sent
 
 if __name__ == "__main__":
-	print("commented out :)")
-	# send2wheels(4, 7)
-	# for i in range(10):
-	# 	test_both_wheels()
-	# 	test_left_wheels()
-	# 	test_right_wheels()
+	Controller()
