49f9a715d39c1dbb0411406c2c24ad48d917c962
Andrew Kosikowski <kosikoaj@rose-hulman.edu>
2023-09-18 20:18:45 -0400

    Updated readme and removed temp code

--- user/wheelcommand.py
+++ user/wheelcommand.py
@@ -1,9 +1,1 @@
-import requests
-
-def send2wheels(l, r):
-	r = requests.get('http://rhitrover1-desktop:8080/wheel_command', json={
-		"left": l, "right": r
-	})
-
-if __name__ == "__main__":
-	send2wheels(4, 7)
+# See WheelCommandDebug.py in arduino/tests/WheelCommandDebug.py
