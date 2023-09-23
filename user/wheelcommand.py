102c7ee511ab82fc43a82a0e3a7baa9121982e16
Andrew Kosikowski <kosikoaj@rose-hulman.edu>
2023-09-23 13:10:08 -0400

    aaaaha wheelcommand fix

--- user/wheelcommand.py
+++ user/wheelcommand.py
@@ -1,15 +1,15 @@
 import requests
 
 def send2wheels_both(l, r):
-	r = requests.get('192.168.0.12:8080/wheel_command_both', json={
+	req = requests.get('http://192.168.0.12:8080/wheel_command_both', json={
 		"left": l, "right": r
 	})
 def send2wheels_left(l):
-	r = requests.get('192.168.0.12:8080/wheel_command_left', json={
+	req = requests.get('http://192.168.0.12:8080/wheel_command_left', json={
 		"left": l
 	})
 def send2wheels_right(r):
-	r = requests.get('192.168.0.12:8080/wheel_command_right', json={
+	req = requests.get('http://192.168.0.12:8080/wheel_command_right', json={
 		"right": r
 	})
 	
