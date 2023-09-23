3389a077ba7341ead917f43ac2ea8be86a26eeab
Evan <obrienem@rose-hulman.edu>
2023-09-23 19:22:05 -0400

    return file descriptor on success

--- jetson/port_grep.py
+++ jetson/port_grep.py
@@ -3,11 +3,10 @@ import serial.tools.list_ports
 
 def find(manufacturer):
 	ports = serial.tools.list_ports.comports()
-	print(ports)
 	for port in ports:
-		print(port.manufacturer)
 		if port.manufacturer == manufacturer:
-			print(True)
+			return port.device
+	raise Exception("device not detected")
 
 
 if __name__ == '__main__':
