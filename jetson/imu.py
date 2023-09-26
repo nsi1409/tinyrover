988477ff9f070def5afa74d9261bbb656bd8c652
rhit-moreholm <moreholm@rose-hulman.edu>
2023-09-25 20:36:53 -0400

    IMU parsed into some numbers

--- jetson/imu.py
+++ jetson/imu.py
@@ -1,50 +1,25 @@
+import socket
 import time
 import serial
 import pywitmotion as wit
 
-# connected = False
-# port = 'COM3'
-# baud = 9600
+from port_grep import find, list_all
 
-# with serial.Serial(port, baud, timeout=5) as ser:
-#     s = ser.read()
-
-#     msgs_num = 0
-#     while msgs_num < 100:
-        
-#         start = time.time()
-#         s = ser.read_until(b'U')
-#         q = wit.get_quaternion(msgs_num)
-#         # q = wit.get_magnetic(msg)
-#         # q = wit.get_angle(msg)
-#         # q = wit.get_gyro(msg)
-#         # q = wit.get_acceleration(msg)
-#         if q is not None:
-#             msgs_num = msgs_num+1
-#             print(q)
-
-# from witmotion import IMU
-
-# def callback(msg):
-#     print(msg)
-
-# imu = IMU()
-# imu.subscribe(callback)
+# port = find(list_all())
+# print(port)
 
 from serial import *
 usb = Serial('COM3', 9600)
+# usb = Serial(port)
 usb.timeout = 1
 while True:
-    print(usb.readline()) # you need to print what you are reading in the script
-    msgs_num = 0
-    while msgs_num < 100:
-        start = time.time()
-        s = usb.read_until(b'U')
-        q = wit.get_quaternion(msgs_num)
-        # q = wit.get_magnetic(msg)
-        # q = wit.get_angle(msg)
-        # q = wit.get_gyro(msg)
-        # q = wit.get_acceleration(msg)
-        if q is not None:
-            msgs_num = msgs_num+1
-            print(q)
+    # print(usb.readline()) # you need to print what you are reading in the script
+    s = usb.read_until(b'U')
+    # for msg in s:c
+    # q = wit.get_quaternion(s)
+    # q = wit.get_angle(s)
+    # q = wit.get_gyro(s)
+    # q = wit.get_acceleration(s)
+    q = wit.get_magnetic(s)
+    if q is not None:
+        print(q)
