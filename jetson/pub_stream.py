fe6e224b01dd9dca37a6bf579f23b8e8d25ef89e
Evan <obrienem@rose-hulman.edu>
2023-12-09 12:38:00 -0500

    try catch to make starting stream in any order

--- jetson/pub_stream.py
+++ jetson/pub_stream.py
@@ -9,10 +9,15 @@ HOST = ''
 PORT = 8089
 cap=cv2.VideoCapture(0)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(10)
-conn, addr = s.accept()
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen(10)
+        conn, addr = s.accept()
+        break
+    except:
+        print('failed to connect')
 
 while True:
 	ret,frame=cap.read()
