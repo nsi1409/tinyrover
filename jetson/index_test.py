d80d63e1909d129132721bf0933830dfdd831100
Evan <obrienem@rose-hulman.edu>
2024-01-13 12:00:37 -0500

    device index finding utility added

---
+++ jetson/index_test.py
@@ -0,0 +1,13 @@
+import cv2
+
+index = 0
+arr = []
+for i in range(10):
+    cap = cv2.VideoCapture(index)
+    if not cap.read()[0]:
+        break
+    else:
+        arr.append(index)
+    cap.release()
+    index += 1
+print(arr)
