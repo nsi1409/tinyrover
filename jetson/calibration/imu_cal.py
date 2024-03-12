e98a9ef6598ee15651104470267ffb0a000b0a95
EricRiddle9185 <riddleet@rose-hulman.edu>
2024-03-11 20:34:26 -0400

    imu cal test

--- jetson/calibration/imu_cal.py
+++ jetson/calibration/imu_cal.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append("..")
+
 import math
 import time
 import serial
@@ -8,6 +11,11 @@ from kv import send_kv
 
 port = port_grep.find(6790)
 usb = Serial(port, 9600, timeout=1)
+
+minX = math.inf
+maxX = -math.inf
+minY = math.inf
+maxY = -math.inf
 
 while True:
 	s = usb.read_until(b'U')
@@ -20,9 +28,18 @@ while True:
 				rectifiedx = magx+5000
 				rectifiedy = magy
 				scuffed_yaw = math.atan2(rectifiedy, rectifiedx)
-				print(scuffed_yaw)
+				# print(scuffed_yaw)
+				if (magx < minX):
+					minX = magx
+				if (magx > maxX):
+					maxX = magx
+				if (magy < minY):
+					minY = magy
+				if (magy > maxY):
+					maxY = magy
 				# print("magnetic: " + str(s))
 				# print(str(magx) + " " + str(magy) + " " + str(magz))
+				print("min x: " + str(minX) + ", max x: " + str(maxX) + ", min y: " + str(minY) + ", max y: " + str(maxY))
 	except Exception as e:
 		print(e)
 		print('reading loop fail, retrying')
