2beb390770e4777f0cbee96083a526282c105d5a
Evan <obrienem@rose-hulman.edu>
2024-04-27 13:05:21 -0400

    convert sensor data to desired range

--- jetson/autonomous/turn.py
+++ jetson/autonomous/turn.py
@@ -5,13 +5,15 @@ import sys
 sys.path.append("..")
 from kv import grab_kv, grab_brown
 
-def turn(location, target): #assumes all angles to be in the range 0, 2*pi (or tau), with numbers assending going clockwise
+def turn(location, target): # assumes all angles to be in the range 0, 2*pi (or tau), with numbers assending going clockwise
 	left = 0
 	right = 0
 	while(True):
 		target_heading = math.atan2(target[0]-location[0], target[1]-location[1])
 		try:
-			heading = grab_kv('scuffed_yaw')['v']
+			heading = grab_kv('scuffed_yaw')['v'] # comes in range -pi, pi 
+			heading += math.pi # these lines convert to range 0, 2pi
+			heading %= 2*math.pi
 			print(heading)
 		except:
 			heading, _, _, _ = grab_brown()
@@ -22,6 +24,7 @@ def turn(location, target): #assumes all angles to be in the range 0, 2*pi (or t
 		angle_right = target_heading - heading
 		difference = abs(angle_right)
 		if difference < (math.pi/6):
+			print('close enough')
 			break
 		elif angle_right < math.pi:
 			left = 1
@@ -34,6 +37,7 @@ def turn(location, target): #assumes all angles to be in the range 0, 2*pi (or t
 
 
 if __name__ == '__main__':
-	location = [random.uniform(0, 60), random.uniform(0, 60)]
-	target = [random.uniform(0, 60), random.uniform(0, 60)]
-	turn(location, target)
+	while(1):
+		location = [random.uniform(0, 60), random.uniform(0, 60)]
+		target = [random.uniform(0, 60), random.uniform(0, 60)]
+		turn(location, target)
