4895aa63821fb0919b4953f79cfb9989ac437d92
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2024-09-21 13:04:33 -0400

    completed initial version of heartbeat system

--- jetson/check_heartbeat.py
+++ jetson/check_heartbeat.py
@@ -1,12 +1,28 @@
 import time
 from kv import grab_kv
+from kv import default_value
+import requests
+
+allowed_flatline_time_ms = 4500
+time_between_listens_ms = 800
 
 while 1:
-	last_beat = grab_kv('heartbeat')
-	print(last_beat)
-	time.sleep(1)
-	#TODO for Ian: implement pseudo code
-	'''
-	if last_beat more than 5 seconds ago:
-		send (90, 90) to localhost/wheel_command using requests library (check the readme to see how to do this)
-	'''
+    heartbeat_value = grab_kv('heartbeat')
+    if(heartbeat_value == default_value):
+        print('waiting for heartbeat...')
+        time.sleep(time_between_listens_ms / 1000)
+        continue
+    last_heard_beat_time = heartbeat_value.get('v')
+    print('last heard heartbeat at time ' + str(last_heard_beat_time))
+    current_time = time.time() * 1000
+    
+    if (current_time - last_heard_beat_time) > allowed_flatline_time_ms:
+        try:
+            print('sending stop command to wheels after not hearing heartbeat')
+            r = requests.get('http://localhost:8080/wheel_command', json={
+            	"left": 90, "right": 90
+            }, timeout=1)
+        except:
+            print('failed to send stop to wheels')
+    
+    time.sleep(time_between_listens_ms / 1000)
--- user/set_heartbeat.py
+++ user/set_heartbeat.py
@@ -3,5 +3,8 @@ sys.path.append('../jetson')
 import time
 from kv import send_kv
 
+heartbeat_send_interval_ms = 400
+
 while 1:
-	send_kv('heartbeat', time.time())
+    send_kv('heartbeat', int(time.time() * 1000))
+    time.sleep(heartbeat_send_interval_ms / 1000)
