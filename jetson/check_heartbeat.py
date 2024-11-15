ad320ab38402d97dadc6d6edbaada81e1abce435
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2024-11-14 20:25:14 -0500

    rewrote check_heartbeat.py
    works on localhost, needs more testing
    based on 78c0febe6ccc7dfd999aab34a0fcca7383d43a0e

--- jetson/check_heartbeat.py
+++ jetson/check_heartbeat.py
@@ -8,9 +8,9 @@ time_between_listens_ms = 800
 
 most_recent_heard = None
 most_recent_beat = None
-second_most_recent_beat = None
+most_recent_check = None
 
-#TODO: fix this
+wheel_stop_sent = False
 
 while 1:
 	heartbeat_value = grab_kv('heartbeat')
@@ -18,28 +18,42 @@ while 1:
 		print('waiting for heartbeat...')
 		time.sleep(time_between_listens_ms / 1000)
 		continue
-
-	current_time = int(time.time() * 1000)
-	most_recent_heard = heartbeat_value.get('v')    
-	last_beat_time = current_time + most_recent_heard
-
-	if(not most_recent_beat):
-		print('heard first beat')
-		most_recent_beat = last_beat_time
-		time.sleep(time_between_listens_ms / 1000)
-		continue
-	if (last_beat_time != most_recent_beat):
-		second_most_recent_beat = most_recent_beat
-		most_recent_beat = last_beat_time
-		print('heard heartbeat at time ' + str(last_beat_time))
-	print('difference between beats is ' + str(most_recent_beat - second_most_recent_beat))
-	if (most_recent_beat - second_most_recent_beat) > allowed_flatline_time_ms:
-		try:
-			print('sending stop command to wheels after not hearing heartbeat')
-			r = requests.get('http://localhost:8080/wheel_command', json={
-				"left": 90, "right": 90
-			}, timeout=1)
-		except:
-			print('failed to send stop to wheels')
 	
-	time.sleep(time_between_listens_ms / 1000)
+	updated_this_cycle = False #used for debugging
+ 
+	current_time = int(time.time() * 1000)
+	if heartbeat_value.get('v') != most_recent_heard:
+		most_recent_heard = heartbeat_value.get('v')
+		most_recent_beat = most_recent_heard + current_time
+		print('heard new heartbeat: ' + str(most_recent_beat))
+		updated_this_cycle = True
+  	
+	most_recent_check = most_recent_heard + current_time
+	
+ 	#used for debugging
+	if not updated_this_cycle:
+		timediff = int(allowed_flatline_time_ms - (most_recent_check - most_recent_beat))
+		print('checked for heartbeat but didn\'t hear it. will stop wheels if don\'t hear another beat in ~' + str(timediff) + ' ms')
+ 
+	if (most_recent_check - most_recent_beat) > allowed_flatline_time_ms:
+		if wheel_stop_sent:
+			time.sleep(time_between_listens_ms / 1000)
+		else:
+			try:
+				print('sending stop command to wheels after not hearing heartbeat')
+				time_before_req = int(time.time() * 1000)
+				r = requests.get('http://localhost:8080/wheel_command', json={
+					"left": 90, "right": 90
+				}, timeout=1)
+				wheel_stop_sent = True
+				time_after_req = int(time.time() * 1000)
+				time_difference = time_after_req - time_before_req
+				if time_difference <= time_between_listens_ms:
+					time.sleep((time_between_listens_ms - time_difference) / 1000)
+			except:
+				print('failed to send stop to wheels')
+				wheel_stop_sent = False
+				time.sleep(time_between_listens_ms / 1000)
+	else:
+		wheel_stop_sent = False
+		time.sleep(time_between_listens_ms / 1000)
