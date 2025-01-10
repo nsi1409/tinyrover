0e1203329eb27f57ddf3a4407316bcc12dcc985d
rhit-leslienk <leslienk@rose-hulman.edu>
2025-01-09 19:02:53 -0500

    Update smart command requests

--- jetson/smart_commands.py
+++ jetson/smart_commands.py
@@ -4,8 +4,6 @@ import argparse
 import requests
 from simple_pid import PID
 
-from wheels import wheel_both, wheel_stop
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--emulate', action='store_true')
 options = parser.parse_args()
@@ -37,14 +35,27 @@ def wheel_turn_both():
         target = request.json['target']
     target = float(target)
 
-    pid = PID(1, 1, 1, setpoint=target)
-    pid.output_limits = (45, 135)
+    """
+    Notes for PID outputs:
+    
+    Kp shouldn't be more than 1/(max error)
+    Start with a very small Ki/Kd
+    
+    Tuning method 1:
+    Let Kd be 0, increase Kp until just slightly undershooting,
+    increase Ki until perfect
+    
+    Tuning method 2:
+    Let Ki be 0, increase Kp until just slightly overshooting,
+    increase Kd until perfect
+    """
+    pid = PID(.0001, 0, 0, setpoint=target)
+    pid.output_limits = (0, 180)
     step = 0
 
     while True:
-        r = requests.get('http://127.0.0.1:5001/data', json={'k': 'scuffed_yaw'})
+        r = requests.get('http://127.0.0.1:5001/data', timeout=3, json={'k': 'scuffed_yaw'})
         yaw = r.json()['v']
-        yaw = (180 * yaw) + 180
         distance_right = (yaw - target) % 360
         distance_left = (target - yaw) % 360
         if distance_right > distance_left:
@@ -53,21 +64,23 @@ def wheel_turn_both():
             distance = -1 * distance_right
 
         if abs(distance) < 5:
+            r = requests.get('http://127.0.0.1:8080/wheel_command_stop', timeout=3)
             return [yaw, target, distance, step]
 
         control = pid(distance)
-        if distance_right > distance_left:
-            left = ((360 - control) / 360) * 90 - 90
-            right = (control / 360) * 90 + 90
-        else:
-            left = (control / 360) * 90 + 90
-            right = ((360 - control) / 360) * 90 - 90
 
-        left = min(left, 135)
-        left = max(left, 45)
-        right = min(right, 135)
-        right = max(right, 45)
-        r = requests.get('http://127.0.0.1:8080/wheel_command_both', json={'left': int(left), 'right': int(right)})
+        if distance >= 0:
+            left = 135 - (.5 * control)
+            right = 135
+        else:
+            left = 135
+            right =  135 - (.5 * control)
+
+        #These lines are redundant but included just to be safe
+        left = min(135, max(45, left))
+        right = min(135, max(45, right))
+
+        r = requests.get('http://127.0.0.1:8080/wheel_command_both', timeout=3, json={'left': int(left), 'right': int(right)})
         step += 1
 
 @app.route('/drivestraight', methods=['GET', 'POST', 'PUT'])
@@ -84,13 +97,12 @@ def wheel_straight_both():
     start_time = time.time()
     elapsed_time = 0
 
-    r = requests.get('http://127.0.0.1:5001/brownsleep', timeout=5)
-    target_angle = r.json()[0]
+    r = requests.get('http://127.0.0.1:5001/data', timeout=3, json={'k': 'scuffed_yaw'})
+    target_angle = r.json()['v']
 
-    while (elapsed_time < target_time):
-        r = requests.get('http://127.0.0.1:5001/brownsleep', timeout=5)
-        yaw = r.json()[0]
-        yaw = (180 * yaw) + 180
+    while elapsed_time < target_time:
+        r = requests.get('http://127.0.0.1:5001/data', timeout=3, json={'k': 'scuffed_yaw'})
+        yaw = r.json()['v']
         distance_right = (yaw - target_angle) % 360
         distance_left = (target_angle - yaw) % 360
         if distance_right > distance_left:
@@ -98,12 +110,15 @@ def wheel_straight_both():
         else:
             distance = -1 * distance_right
         if abs(distance) > 5:
-            wheel_turn_both()
+            r = requests.get('http://127.0.0.1:8080/wheel_command_stop', timeout=3)
+            r = requests.get('http://192.168.0.12:8081/turn', timeout=3, json={"target": target_angle})
+            target_time += (time.time() - elapsed_time)
         
         elapsed_time = time.time() - start_time
-        wheel_both(velocity*90+90, velocity*90+90)
+        control = max(45, min(135, (velocity * 90) + 90))
+        r = requests.get('http://127.0.0.1:8080/wheel_command_both', timeout=3, json={'left': int(control), 'right': int(control)})
         
-    wheel_stop()
+    r = requests.get('http://127.0.0.1:8080/wheel_command_stop', timeout=3)
 
         
 
