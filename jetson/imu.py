95585292139df5956d024ad1225da19482900466
The Rover <obrienem@rose-hulman.edu>
2025-01-16 19:22:52 -0500

    Remove match statement

--- jetson/imu.py
+++ jetson/imu.py
@@ -30,34 +30,31 @@ while True:
 			usb = Serial(port, 9600, timeout=1)
 			connected = True
 		s = usb.read_until(b'U')
-		match s[0]:
-			case 83: #euler angles
-				(rollr, pitchr, yawr) = struct.unpack("<hhh", s[1:7])
-				roll = (rollr / 32768) * 180
-				pitch = (pitchr / 32768) * 180
-				yaw = (yawr / 32768) * 180 + 180
-				send_kv('yaw', yaw)
-				send_kv('roll', roll)
-				send_kv('pitch', pitch)
-				send_kv('euler', [roll, pitch, yaw])
-			case 84: #magnetic field
-				(magx, magy, magz) = struct.unpack("<hhh", s[1:7])
-				x_range = max_x - min_x
-				y_range = max_y - min_y
-				adjusted_x = (2 * ((magx - min_x) / x_range)) - 1
-				adjusted_x = adjusted_x * -1
-				adjusted_y = (2 * ((magy - min_y) / y_range)) - 1
-				scuffed_yaw = math.atan2(adjusted_x, adjusted_y)
-				send_kv('scuffed_yaw', scuffed_yaw)
-			case 89: #quaternion
-				qr = struct.unpack("<hhhh", s[1:9])
-				q = tuple(el / 32768 for el in qr)
-				send_kv('quat', q)
-			case 87: #latitude and longitude
-				(longu, longl, latu, latl) = struct.unpack("<hhhh", s[1:9])
-				# send_kv('lat long', l)
-			case _: #default case
-				pass
+		if s[0] == 83: #euler angles
+			(rollr, pitchr, yawr) = struct.unpack("<hhh", s[1:7])
+			roll = (rollr / 32768) * 180
+			pitch = (pitchr / 32768) * 180
+			yaw = (yawr / 32768) * 180 + 180
+			send_kv('yaw', yaw)
+			send_kv('roll', roll)
+			send_kv('pitch', pitch)
+			send_kv('euler', [roll, pitch, yaw])
+		if s[0] == 84: #magnetic field
+			(magx, magy, magz) = struct.unpack("<hhh", s[1:7])
+			x_range = max_x - min_x
+			y_range = max_y - min_y
+			adjusted_x = (2 * ((magx - min_x) / x_range)) - 1
+			adjusted_x = adjusted_x * -1
+			adjusted_y = (2 * ((magy - min_y) / y_range)) - 1
+			scuffed_yaw = math.atan2(adjusted_x, adjusted_y)
+			send_kv('scuffed_yaw', scuffed_yaw)
+		if s[0] == 89: #quaternion
+			qr = struct.unpack("<hhhh", s[1:9])
+			q = tuple(el / 32768 for el in qr)
+			send_kv('quat', q)
+		if s[0] == 87: #latitude and longitude
+			(longu, longl, latu, latl) = struct.unpack("<hhhh", s[1:9])
+			# send_kv('lat long', l)
 		print('imu.py is running...')
 	except SerialException as se:
 		print('imu disconnected, retrying')
