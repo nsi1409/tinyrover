97a5a384b26c55a92e0bbb2a73fb43e0465f5f29
Evan <obrienem@rose-hulman.edu>
2025-02-15 12:35:24 -0500

    elevation api script

---
+++ path_planning/elevation.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_elevation(lat, long):
+	url = 'https://epqs.nationalmap.gov/v1/json'
+	#x = long
+	params = {
+		'x': long,
+		'y': lat,
+		'units': 'Feet',
+		'wkid': 4326,
+		'includeDate': False
+	}
+	r = requests.get(url, params=params)
+	print(r)
+	print(r.text)
+	json = r.json()
+	return json['value']
+
+if __name__ == '__main__':
+	for i in range(10):
+		for j in range(10):
+			lat = 40.015+(0.01*i)
+			long = -105.2705+(0.01*j)
+			print(lat)
+			print(long)
+			print(get_elevation(lat, long))
