2490ad5ed2f29d360644a87c578eedd3069e4b95
rhit-leslienk <leslienk@rose-hulman.edu>
2025-02-22 12:41:25 -0500

    Cast mag and trim to ints

--- jetson/wheels.py
+++ jetson/wheels.py
@@ -59,6 +59,8 @@ def wheel_trim():
 	json = request.json
 	trim = json["trim"]
 	magnitude = json["magnitude"]
+	magnitude = int(magnitude)
+	trim = int(trim)
 	assert magnitude >= -1
 	assert magnitude <= 1
 	if(trim > 0):
@@ -67,8 +69,8 @@ def wheel_trim():
 	else:
 		left = (90 * magnitude) + 90
 		right = (90 * (magnitude * (1 - ((-1) * trim)))) + 90
-	left = min(180, max(0, int(left)))
-	right = min(180, max(0, int(right)))
+	left = min(180, max(0, left))
+	right = min(180, max(0, right))
 	msg = f'trim drive left: {left}, right: {right}'
 	print(msg)
 	j2a.send_both(left, right)
