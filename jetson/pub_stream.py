2a2fea2897c6c239eaa64af933da8fdb1635676b
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2025-03-22 13:21:37 -0400

    set video capture to 1

--- jetson/pub_stream.py
+++ jetson/pub_stream.py
@@ -7,19 +7,19 @@ import struct
 
 HOST = ""
 PORT = 8089
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((HOST, PORT))
-        s.listen(10)
-        conn, addr = s.accept()
-        break
-    except KeyboardInterrupt:
-        exit()
-    except:
-        print("failed to connect")
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind((HOST, PORT))
+		s.listen(10)
+		conn, addr = s.accept()
+		break
+	except KeyboardInterrupt:
+		exit()
+	except:
+		print("failed to connect")
 
 while True:
 	ret, frame = cap.read()
