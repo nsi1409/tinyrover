e203a492d90cb1c9114cf51c966ec6eecfa512b4
rhit-steineig <116207222+rhit-steineig@users.noreply.github.com>
2025-05-08 19:54:05 -0400

    update to wheel control loop

--- arduino/serial_motor/serial_motor.ino
+++ arduino/serial_motor/serial_motor.ino
@@ -1,35 +1,35 @@
 #include <Servo.h>
 #include <TimerOne.h>
-//https://hobbymania.com.ua/file/FlyColor_boat_ESC.pdf
- 
+// https://hobbymania.com.ua/file/FlyColor_boat_ESC.pdf
+
 #define MAX_PULSE 2000
 #define MIN_PULSE 1000
 #define OFF_PULSE 1500
 #define TIME_TO_LIVE_MS 500
- 
+
 String command;
- 
+
 int leftTargetSpeed;
 int leftCurrentSpeed;
 int rightTargetSpeed;
 int rightCurrentSpeed;
 int updateSpeedTimer;
- 
+
 int state[4] = {1, 0, 1, 0};
 char charBuf[128];
- 
+
 Servo rightFrontServo;
 Servo leftFrontServo;
 Servo leftBackServo;
 Servo rightBackServo;
 Servo leftMiddleServo;
 Servo rightMiddleServo;
- 
+
 size_t wheelCommandBufferSize = 1;
 size_t opCodeSize = 1;
- 
+
 // Unit circle orientation starting at front right
-const int ipn[6] = {6,11,10,9,3,5}; //new mega
+const int ipn[6] = {6, 11, 10, 9, 3, 5};  // new mega
 // pin 11 front left
 // pin 6 front right
 // pin 10 middle left
@@ -39,29 +39,27 @@ const int ipn[6] = {6,11,10,9,3,5}; //new mega
 const int configPin = 12;
 unsigned long received_time;
 unsigned long current_time;
-bool received_time_defined = false;
- 
+
 void setup() {
   for (int pin : ipn) {
     pinMode(pin, OUTPUT);
   }
   pinMode(configPin, INPUT);
- 
+
   leftTargetSpeed = OFF_PULSE;
   leftCurrentSpeed = OFF_PULSE;
   rightTargetSpeed = OFF_PULSE;
   rightCurrentSpeed = OFF_PULSE;
- 
- 
+
   updateSpeedTimer = 0;
- 
+
   Serial.begin(9600);
-  Serial.setTimeout(100); // in milliseconds
-  
+  Serial.setTimeout(100);  // in milliseconds
+
   initializeMotors();
- 
-  Timer1.initialize(3000); //Start timer to have 3000 MICROseconds period
-  Timer1.attachInterrupt(updateMotors); //Attach function to run every time period completion
+
+  Timer1.initialize(3000);               // Start timer to have 3000 MICROseconds period
+  Timer1.attachInterrupt(updateMotors);  // Attach function to run every time period completion
 }
 
 void initializeMotors() {
@@ -71,53 +69,53 @@ void initializeMotors() {
   rightMiddleServo.attach(ipn[5], MIN_PULSE, MAX_PULSE);
   leftBackServo.attach(ipn[3], MIN_PULSE, MAX_PULSE);
   rightBackServo.attach(ipn[4], MIN_PULSE, MAX_PULSE);
- 
-  //ONLY UNCOMMENT ONE OF THESE SETUP FUNCTIONS AT A TIME
- 
-  //Set throttle range
-  // resetThrottleRangeForMotors();
- 
+
+  // ONLY UNCOMMENT ONE OF THESE SETUP FUNCTIONS AT A TIME
+
+  // Set throttle range
+  //  resetThrottleRangeForMotors();
+
   // Change motor to drive forwards and backwards
   // setMotorsToForwardsAndBackwards();
- 
-  //Already setup, normal working mode
+
+  // Already setup, normal working mode
   normalWorkingMode();
- 
+
   if (digitalRead(configPin) == HIGH) {
     resetThrottleRangeForMotors();
   } else {
     normalWorkingMode();
   }
 }
- 
-void normalWorkingMode(){
+
+void normalWorkingMode() {
   moveAllWheels(OFF_PULSE);
 }
- 
-void setMotorsToForwardsAndBackwards(){
+
+void setMotorsToForwardsAndBackwards() {
   moveAllWheels(MAX_PULSE);
- 
+
   delay(9000);
- 
+
   moveAllWheels(OFF_PULSE);
- 
+
   delay(7000);
- 
+
   moveAllWheels(MAX_PULSE);
- 
+
   delay(2000);
- 
+
   moveAllWheels(OFF_PULSE);
 }
- 
-void resetThrottleRangeForMotors(){
+
+void resetThrottleRangeForMotors() {
   moveAllWheels(1600);
- 
+
   delay(5000);
- 
+
   moveAllWheels(OFF_PULSE);
 }
- 
+
 // Debugging, printing is iffy regardless, can just be safer to use Serial.write
 //  void printBufferIntArray(uint8_t* array, size_t bufferSize){
 //    Serial.print("[");
@@ -125,7 +123,7 @@ void resetThrottleRangeForMotors(){
 //      Serial.print(array[i]);
 //      Serial.print(",");
 //    }
- 
+
 //   Serial.print("]\n");
 // }
 
@@ -137,14 +135,13 @@ void moveAllWheels(int speed) {
   leftBackServo.writeMicroseconds(speed);
   rightBackServo.writeMicroseconds(speed);
 }
- 
+
 void moveLeftWheel(int speed) {
-  //Flip orientation as we consider the right wheels the dominant speed
+  // Flip orientation as we consider the right wheels the dominant speed
   int difference_from_middle = speed - OFF_PULSE;
-  int corrected_speed = speed - (2*difference_from_middle);
+  int corrected_speed = speed - (2 * difference_from_middle);
   // int corrected_speed=speed;
- 
- 
+
   leftFrontServo.writeMicroseconds(corrected_speed);
   leftBackServo.writeMicroseconds(corrected_speed);
   leftMiddleServo.writeMicroseconds(corrected_speed);
@@ -152,30 +149,29 @@ void moveLeftWheel(int speed) {
   // leftMiddleServo.write(speed);
   // leftBackServo.write(speed);
 }
- 
+
 int convertSpeedToProperRange(uint8_t speed) {
-  //For write microseconds
-  float conv_speed = (float) speed;
+  // For write microseconds
+  float conv_speed = (float)speed;
   int converted = (conv_speed) / 180 * (MAX_PULSE - MIN_PULSE);
   int result = converted + MIN_PULSE;
   return result;
- 
-  //For write
-  // return speed;
+
+  // For write
+  //  return speed;
 }
- 
+
 void moveRightWheel(int speed) {
   // rightFrontServo.write(speed);
   // rightMiddleServo.write(speed);
   // rightBackServo.write(speed);
- 
+
   rightFrontServo.writeMicroseconds(speed);
   rightMiddleServo.writeMicroseconds(speed);
   rightBackServo.writeMicroseconds(speed);
 }
- 
-void updateMotors(){
- 
+
+void updateMotors() {
   if (leftCurrentSpeed != leftTargetSpeed) {
     if (leftCurrentSpeed < leftTargetSpeed) {
       leftCurrentSpeed = leftCurrentSpeed + 1;
@@ -185,7 +181,7 @@ void updateMotors(){
     moveLeftWheel(leftCurrentSpeed);
     //
   }
- 
+
   if (rightCurrentSpeed != rightTargetSpeed) {
     if (rightCurrentSpeed < rightTargetSpeed) {
       rightCurrentSpeed = rightCurrentSpeed + 1;
@@ -194,62 +190,39 @@ void updateMotors(){
     }
     moveRightWheel(rightCurrentSpeed);
   }
- 
 }
- 
+
 void loop() {
   current_time = millis();
-  if(received_time_defined) { // this stops the rover if no wheel signal received for half a second
-    if(current_time - received_time >= TIME_TO_LIVE_MS) {
-      //stop
-      received_time_defined = false;
-      leftTargetSpeed = convertSpeedToProperRange(90);
-      rightTargetSpeed = convertSpeedToProperRange(90);
-    }
-  }
   if (Serial.available() > 0) {
-    received_time_defined = true;
     received_time = millis();
     uint8_t opBuffer[opCodeSize];
-    Serial.readBytes(
-      opBuffer,
-      opCodeSize); // Reads as many bytes or until port closes
+    Serial.readBytes(opBuffer, opCodeSize);  // Reads as many bytes or until port closes
     if (opBuffer[0] == 0) {
-      // if(Serial.available() > wheelCommandBufferSize){
       uint8_t leftWheelBuffer[wheelCommandBufferSize];
-      Serial.readBytes(leftWheelBuffer,
-               wheelCommandBufferSize); // Reads as many bytes or
-                            // until port closes
- 
+      Serial.readBytes(leftWheelBuffer, wheelCommandBufferSize);
+
       uint8_t rightWheelBuffer[wheelCommandBufferSize];
-      Serial.readBytes(rightWheelBuffer,
-               wheelCommandBufferSize); // Reads as many bytes or
-                            // until port closes
- 
+      Serial.readBytes(rightWheelBuffer, wheelCommandBufferSize);
+
       leftTargetSpeed = convertSpeedToProperRange(leftWheelBuffer[0]);
       rightTargetSpeed = convertSpeedToProperRange(rightWheelBuffer[0]);
-      // }
     } else if (opBuffer[0] == 1) {
-      //  if (Serial.available() > wheelCommandBufferSize -1) {
       uint8_t leftWheelBuffer[wheelCommandBufferSize];
-      Serial.readBytes(leftWheelBuffer,
-               wheelCommandBufferSize); // Reads as many bytes or
-                            // until port closes
- 
+      Serial.readBytes(leftWheelBuffer, wheelCommandBufferSize);
+
       leftTargetSpeed = convertSpeedToProperRange(leftWheelBuffer[0]);
- 
-      // }
     } else if (opBuffer[0] == 2) {
-      // if (Serial.available() > wheelCommandBufferSize -1) {
       uint8_t rightWheelBuffer[wheelCommandBufferSize];
-      Serial.readBytes(rightWheelBuffer,
-               wheelCommandBufferSize); // Reads as many bytes or
-                            // until port closes
- 
+      Serial.readBytes(rightWheelBuffer, wheelCommandBufferSize);
+
       rightTargetSpeed = convertSpeedToProperRange(rightWheelBuffer[0]);
-      // }
     } else {
       Serial.flush();
     }
+  }
+  if (current_time - received_time >= TIME_TO_LIVE_MS) {
+    leftTargetSpeed = convertSpeedToProperRange(90);
+    rightTargetSpeed = convertSpeedToProperRange(90);
   }
 }
